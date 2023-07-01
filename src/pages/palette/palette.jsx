a34d0ee178793b126298834a5048b93229ca8f7b
单志林 <940259702@qq.com>
2023-07-01 22:10:54 +0800

    feat: 底部版权

--- src/pages/palette/palette.jsx
+++ src/pages/palette/palette.jsx
@@ -5,17 +5,22 @@ import Options from "./components/options/options";
 
 function Palette() {
   return (
-    <div className={styles.palette_con}>
-      <div className={styles.palette_left}>
-        <DrawBtns />
+    <div className={styles.palette_box}>
+      <div className={styles.palette_con}>
+        <div className={styles.palette_left}>
+          <DrawBtns />
+        </div>
+        <div className={styles.palette_right}>
+          <div className={styles.palette_disposition}>
+            <Options />
+          </div>
+          <div className={styles.palette_canvas}>
+            <canvas id="canvas" width="1200px" height="350px"></canvas>
+          </div>
+        </div>
       </div>
-      <div className={styles.palette_right}>
-        <div className={styles.palette_disposition}>
-          <Options />
-        </div>
-        <div className={styles.palette_canvas}>
-          <canvas id="canvas" width="1200px" height="350px"></canvas>
-        </div>
+      <div className={styles.palette_bottom}>
+        <a href="https://beian.miit.gov.cn/" target="_blank" rel="noreferrer">皖ICP备2023010880号-1</a>
       </div>
     </div>
   );
